46f30660059aab51d33c6968af87d31962891792
venca1 <waclav.petrak@gmail.com>
2025-11-04 08:53:15 +0100

    ullog

--- WencaRCH/popup.js
+++ WencaRCH/popup.js
@@ -68,6 +68,31 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     }
+        // --- Akce pro Tlačítko tlacitkoUloz2 (Spustí Content Script) ---
+    const tlacitkoUloz2 = document.getElementById('tlacitkoUloz2');
+    if (tlacitkoUloz2) {
+        tlacitkoUloz2.addEventListener('click', function() {
+          //  potvrzeniElement.textContent = `Aktivována funkce pro Tlačítko 2: Mění vel2.`;
+
+            // Používáme chrome.scripting.executeScript pro spuštění zmenaVelikostTabodoo.js
+            chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+                const activeTab = tabs[0];
+                
+               chrome.scripting.executeScript({
+                    target: { tabId: activeTab.id },
+                    files: ['ulozTabodoo2.js'] 
+                })
+                .then(() => {console.log("Spuštění Uložení skriptu úspěšné.");
+            potvrzeniElement.innerHTML = "Spuštění Uložení skriptu úspěšné."
+        })
+                .catch(err => {
+                    console.error("Chyba při spouštění skriptu z popup:", err);
+                    potvrzeniElement.innerHTML = "Chyba při Spuštění Uložení!"
+                });
+
+            });
+        });
+    }
     // --- Akce pro Tlačítko 3, 4 (Pouze potvrdí kliknutí) ---
     const tlacitkaAkce = [
         document.getElementById('tlacitko3'),
